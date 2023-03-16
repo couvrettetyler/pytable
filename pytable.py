4b2ccd169e690bafbad49ab9f673116b52235fd0
Tyler Couvrette <tyler@5thgait.com>
2023-03-16 14:32:15 -0600

    updated pytable to input a delimter. default is ','

--- pytable.py
+++ pytable.py
@@ -1,10 +1,11 @@
 class pytable():
-	def __init__(self, save_file):
+	def __init__(self, save_file, delimiter = ','):
 		self.text_to_append = ''
 		self.save_file = save_file
 		self.first_data = True
 		self.headers = []
 		self.rewrite = True
+		self.delimiter = delimiter
 
 	def save(self):
 		if self.rewrite:
@@ -23,8 +24,8 @@ class pytable():
 			new_line = ''
 			for key, value in data.items():
 				self.headers.append(key)
-				first_line += str(key) + ','
-				new_line += str(value) + ','
+				first_line += str(key) + self.delimiter
+				new_line += str(value) + self.delimiter
 			first_line = first_line[:-1] + '\n'
 			new_line = new_line[:-1] + '\n'
 			self.text_to_append += first_line + new_line
@@ -33,9 +34,9 @@ class pytable():
 			new_line = ''
 			for key in self.headers:
 				if key in data.keys():
-					new_line += str(data[key]) + ','
+					new_line += str(data[key]) + self.delimiter
 				else:
-					new_line += '!,'
+					new_line += f'!{self.delimiter}'
 
 			new_keys = []
 			for key in data.keys():
@@ -47,19 +48,19 @@ class pytable():
 					not_saved_lines = self.text_to_append.split('\n')
 
 					#update headers and first line
-					self.headers = not_saved_lines[0].split(',') + new_keys
+					self.headers = not_saved_lines[0].split(self.delimiter) + new_keys
 					not_saved_lines[0] = ''
 					for key in self.headers:
-						not_saved_lines[0] += str(key) + ','
+						not_saved_lines[0] += str(key) + self.delimiter
 					not_saved_lines[0] = not_saved_lines[0][:-1] + '\n'
 
 					#update all other lines with placeholder ('!')
 					for i in range(len(not_saved_lines) - 1):
-						not_saved_lines[i+1] = not_saved_lines[i+1][:-1] + (',!'*len(new_keys)) + '\n'
+						not_saved_lines[i+1] = not_saved_lines[i+1][:-1] + (f'{self.delimiter}!'*len(new_keys)) + '\n'
 
 					#finish generating new line with remaining keys
 					for key in new_keys:
-						new_line += str(data[key]) + ','
+						new_line += str(data[key]) + self.delimiter
 					new_line = new_line[:-1] + '\n'
 
 					#peice it all back together
@@ -74,24 +75,24 @@ class pytable():
 						lines = file.read().split('\n')
 
 						#update headers and first line
-						self.headers = lines[0].split(',') + new_keys
+						self.headers = lines[0].split(self.delimiter) + new_keys
 						lines[0] = ''
 						for key in self.headers:
-							lines[0] += str(key) + ','
+							lines[0] += str(key) + self.delimiter
 						lines[0] = lines[0][:-1] + '\n'
 
 						#update all other lines with placeholder ('!')
 						for i in range(len(lines) - 1):
-							lines[i+1] = lines[i+1][:-1] + (',!'*len(new_keys)) + '\n'
+							lines[i+1] = lines[i+1][:-1] + (f'{self.delimiter}!'*len(new_keys)) + '\n'
 						not_saved_lines = []
 						if self.text_to_append != '':
 							not_saved_lines = self.text_to_append.split('\n')
 							for i in range(len(not_saved_lines)):
-								not_saved_lines[i] = not_saved_lines[i][:-1] + (',!'*len(new_keys)) + '\n'
+								not_saved_lines[i] = not_saved_lines[i][:-1] + (f'{self.delimiter}!'*len(new_keys)) + '\n'
 
 						#finish generating new line with remaining keys
 						for key in new_keys:
-							new_line += str(data[key]) + ','
+							new_line += str(data[key]) + self.delimiter
 						new_line = new_line[:-1] + '\n'
 
 						#peice it all back together
